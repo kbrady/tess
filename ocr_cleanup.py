4a09f421beeb7d221065bcbc2362f4616c71f020
kbrady <anne.kbrady@gmail.com>
2017-05-17 20:33:55 -0500

    Add ability to clean up lines and words (doesn't consider line position when fixing)

--- ocr_cleanup.py
+++ ocr_cleanup.py
@@ -110,7 +110,11 @@ class Line(Part):
 
 	def assign_matching(self, string):
 		self.updated_line = string
-		assign(self.children, string.split(' '), complete_coverage=True)
+		if len(string) > 0:
+			assign(self.children, string.split(' '), complete_coverage=True)
+		else:
+			for word in self.children:
+				word.assign_matching('')
 
 # An object to interpret hocr files
 class Document:
@@ -152,9 +156,140 @@ class Document:
 		self.root.set('filename', correct_filename)
 		self.correct_lines = correct_lines
 
+	def assign_lines(self):
+		if len(self.correct_lines) == 0:
+			raise RuntimeError('Need to assign correct lines to document')
+		bag_of_lines = [str(l) for l in self.lines]
+		# start by finding the exact matches
+		# currently we expect there to be a lot of these. This might not hold up in the future
+		# other methods may be needed
+		correct_lines = [-1] * len(bag_of_lines)
+		for i in range(len(bag_of_lines)):
+			if bag_of_lines[i] in self.correct_lines:
+				corrected_line_index = self.correct_lines.index(bag_of_lines[i])
+				correct_lines[i] = corrected_line_index
+		# fill in missing lines (for the moment assume no mistakes with the last step)
+		for i in range(len(bag_of_lines)):
+			if correct_lines[i] != -1:
+				continue
+			correct_line_index = self.find_line_to_assign(correct_lines, i)
+			if correct_line_index is None:
+				self.lines[i].assign_matching('')
+				continue
+			distance = self.lines[i].levenshteinDistance(self.correct_lines[correct_line_index])
+			if distance < (len(self.correct_lines[correct_line_index]) * .5):
+				self.lines[i].assign_matching(self.correct_lines[correct_line_index])
+			else:
+				self.lines[i].assign_matching('')
+
+	def find_line_to_assign(self, correct_lines, index, forward=True):
+		iterator = 1 if forward else -1
+		next_found_index = index+iterator
+		while next_found_index < len(correct_lines) and next_found_index >= 0 and correct_lines[next_found_index] == -1:
+			next_found_index += iterator
+		if next_found_index >= 0 and next_found_index < len(self.lines):
+			correct_index = correct_lines[next_found_index] - (next_found_index-index)
+			if correct_index > 0:
+				return correct_index
+			else:
+				return None
+		else:
+			if forward:
+				return self.find_line_to_assign(correct_lines, index, forward=False)
+			else:
+				print correct_lines
+				print self.tesseract_file
+				print self.correct_lines
+				print str(self)
+				print set([str(l) for l in self.lines]) & set(self.correct_lines)
+				raise Exception('No totally correct lines found')
+
 	def save(self):
 		tree = ET.ElementTree(self.root)
 		tree.write(self.xml_file)
+
+# to match words which were incorrect
+def assign(set_to_assign, all_strings, complete_coverage=False):
+	# to sort the words by how well they match
+	def zip_indexes(word_tuple, matching_tuple):
+		if complete_coverage:
+			word_index = word_tuple[0]
+			matching_index = matching_tuple[2]
+			normalized_diff = float(abs(word_index - matching_index))/len(set_to_assign)
+		else:
+			normalized_diff = 0
+		match_diff = matching_tuple[0]
+		return normalized_diff + match_diff
+	def edge_of_assignment(match_index, comparison):
+		return len([i for i in range(len(assignment)) if comparison(i,match_index) and assignment[i] != -1]) == 0
+	words = [(i, set_to_assign[i]) for i in range(len(set_to_assign))]
+	matchings = [w.find_matching(all_strings) for w in set_to_assign]
+	together = zip(words, matchings)
+	together = [(zip_indexes(*tuple(x)), x) for x in together]
+	together.sort()
+	# assign the strings to the words they will be assigned to
+	assignment = [-1] * len(all_strings)
+	for pairing in together:
+		match_index = pairing[1][1][2]
+		word_index = pairing[1][0][0]
+		if assignment[match_index] != -1:
+			continue
+		largest_before = [x for x in assignment[:match_index] if x != -1]
+		if len(largest_before) > 0 and max(largest_before) > word_index:
+			continue
+		smallest_after = [x for x in assignment[match_index:] if x != -1]
+		if len(smallest_after) > 0 and min(smallest_after) < word_index:
+			continue
+		assignment[match_index] = word_index
+	for match_index in range(len(assignment)):
+		# don't find a match if one has already been found
+		if assignment[match_index] != -1:
+			continue
+		# find the latest assigned word before this one
+		before_index = match_index - 1
+		before = None if before_index < 0 else assignment[before_index]
+		while before == -1:
+			before_index -= 1
+			before = None if before_index < 0 else assignment[before_index]
+		# find the earliest assigned word after this one
+		after_index = match_index + 1
+		after = None if after_index >= len(assignment) else assignment[after_index]
+		while after == -1:
+			after_index += 1
+			after = None if after_index >= len(assignment) else assignment[after_index]
+		# if no word was assigned before this one, assign this word to the greater of
+		# the earliest slot found after minus the difference between this word and that one
+		# and 0
+		if before is None:
+			assignment[match_index] = max(after - (after_index - match_index), 0)
+			continue
+		# if no word was assigned after this one, assign  this word to the lesser of
+		# the latest word found before plus the difference between this word and that one
+		# and the last index
+		if after is None:
+			assignment[match_index] = min(before + (match_index - before_index), len(set_to_assign)-1)
+			continue
+		# if a word was found before and a word was found after, get both possible positions
+		# note that our minimum value is before and our maximum value is after
+		# (not 0 and the length of the list)
+		before_val = min(before + (match_index - before_index), after)
+		after_val = max(after - (after_index - match_index), before)
+		# if the two possible positions are the same, assign the word to that position
+		if before_val == after_val:
+			assignment[match_index] = before_val
+		# otherwise assign it to the position it has a closer lexical distance to
+		else:
+			before_diff = set_to_assign[before_val].levenshteinDistance(all_strings[match_index])
+			after_diff = set_to_assign[after_val].levenshteinDistance(all_strings[match_index])
+			if before_diff < after_diff:
+				assignment[match_index] = before_val
+			else:
+				assignment[match_index] = after_val
+	for word_index in range(len(set_to_assign)):
+		condition = lambda match_index: assignment[match_index] == word_index
+		matching_strings = [all_strings[i] for i in range(len(assignment)) if condition(i)]
+		text = ' '.join(matching_strings)
+		set_to_assign[word_index].assign_matching(text)
 
 # get lists of the lines in each correct file
 def get_correct_bags():
@@ -178,6 +313,7 @@ def cleanup(sess):
 			correct_filename, correct_lines = document.find_correct(correct_bags)
 		else:
 			document.assign_correct_bag(correct_filename, correct_lines)
+		document.assign_lines()
 		document.save()
 
 if __name__ == '__main__':

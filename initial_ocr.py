32ce3f99c9db25541a8230bb3d32426a4d875282
kbrady <anne.kbrady@gmail.com>
2017-08-31 16:35:52 -0500

    Retool initial_ocr so it can run tests on non-session images

--- initial_ocr.py
+++ initial_ocr.py
@@ -10,6 +10,8 @@ from scipy import ndimage, misc
 import time
 # to run subprocesses without dealing with spaces etc. by myself
 import subprocess
+# to save the amount of time things take
+import csv
 
 def make_ocr_ready_images(sess, redo=False):
 	digital_reading_times = [x for x in sess.metadata if x['part'] == 'digital reading']
@@ -21,14 +23,21 @@ def make_ocr_ready_images(sess, redo=False):
 			# (this script may be run multiple times if it didn't finish)
 			# saving on image creation and running tesseract will help a lot with time
 			dir_for_bigger_images = sess.dir_name + os.sep + settings.images_ready_for_ocr
-			full_path_for_new_image = dir_for_bigger_images + os.sep + filename
-			if not redo and os.path.isfile(full_path_for_new_image):
-				continue
-			pic = video_to_frames.get_part_of_picture(full_path, x_range=settings.digital_reading_x_range, y_range=settings.digital_reading_y_range)
-			bigger_pic = ndimage.zoom(pic, (2, 2, 1), order=0)
 			if not os.path.isdir(dir_for_bigger_images):
 				os.mkdir(dir_for_bigger_images)
-			misc.imsave(full_path_for_new_image, bigger_pic)
+			full_path_for_new_image = dir_for_bigger_images + os.sep + filename
+			resize_image(full_path, full_path_for_new_image, redo=redo)
+
+def resize_dir(origin_dir, resized_dir, redo=False):
+	for filename in os.listdir(origin_dir):
+		resize_image(origin_dir + os.sep + filename, resized_dir + os.sep + filename, redo=redo)
+
+def resize_image(original_path, resized_path, redo=False):
+	if not redo and os.path.isfile(resized_path):
+		return
+	pic = video_to_frames.get_part_of_picture(original_path, x_range=settings.digital_reading_x_range, y_range=settings.digital_reading_y_range)
+	bigger_pic = ndimage.zoom(pic, (2, 2, 1), order=0)
+	misc.imsave(resized_path, bigger_pic)
 
 def run_tesseract(sess, redo=False):
 	digital_reading_times = [x for x in sess.metadata if x['part'] == 'digital reading']
@@ -52,16 +61,27 @@ def run_tesseract_on_image(image_path, hocr_path, redo=False):
 	command = ['tesseract', image_path, hocr_path, '-l', 'eng', 'hocr']
 	subprocess.call(command)
 
+def run_tesseract_on_dir(picture_dir, hocr_dir, redo=False):
+	for filename in os.listdir(picture_dir):
+		image_path = picture_dir + os.sep + filename
+		hocr_path = hocr_dir + os.sep + '.'.join(filename.split('.')[:-1])
+		run_tesseract_on_image(image_path, hocr_path)
+
 if __name__ == '__main__':
+	resize_dir('test-data/original-pictures', 'test-data/resized-pictures')
+	run_tesseract_on_dir('test-data/resized-pictures', 'test-data/hocr-files')
+	"""
 	# some session ids from the pilot data
 	all_sessions = video_to_frames.get_session_names()
 
-	t0 = time.time()
-	for sess_name in all_sessions:
-		print sess_name
-		sess = video_to_frames.Session(sess_name)
-		make_ocr_ready_images(sess)
-		run_tesseract(sess)
-	t1 = time.time()
-	print 'time taken', t1 - t0
+	with open('tesseract_times.csv', 'w') as outputfile:
+		writer = csv.writer(outputfile, delimiter=',', quotechar='"')
+		writer.writerow(['sess_name', 'time'])
+		for sess_name in all_sessions:
+			t0 = time.time()
+			sess = video_to_frames.Session(sess_name)
+			run_tesseract(sess, redo=True)
+			t1 = time.time()
+			writer.writerow([sess_name, t1 - t0])
+	"""
 

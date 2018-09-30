71941407f8e2249800cf7889500fe94f69021774
kbrady <anne.kbrady@gmail.com>
2018-09-30 14:06:18 -0500

    Clean up files

--- local_paths.py
+++ local_paths.py
@@ -1,6 +1,8 @@
+# set paths
 raw_dirs = []
 raw_dirs.append('/videos')
 data_dir = '/tess/data/'
+
 # highlight colors
 highlight_colors = {'dark blue':(71,148,241), 'yellow':(253, 250, 193), 'light blue':(213, 242, 254), 'white':(245, 245, 245), 'black':(90,90,90)}
 highlight_color_pairs = {
@@ -9,3 +11,5 @@ highlight_color_pairs = {
 	'yellow':(highlight_colors['yellow'], highlight_colors['black']),
 	'white':(highlight_colors['white'], highlight_colors['black'])
 }
+
+# functions for disecting videos
--- short_scripts.py
+++ short_scripts.py
@@ -3,11 +3,13 @@ import os
 # to look in the right places
 import settings
 # to get the session info
-from video_to_frames import Session, get_session_names, time_to_filename
+from video_to_frames import Session, get_session_names, time_to_filename, get_session_names
 # to calculate the standard deviation of dimensions
 import numpy as np
 # to copy files
 from shutil import copyfile
+# to record reading times in csv
+import csv
 
 def get_last_frame_files(sess, dir_name='last_frames', part='digital reading'):
 	# get the times for this session
@@ -26,6 +28,26 @@ def get_last_frame_files(sess, dir_name='last_frames', part='digital reading'):
 	# copy the files
 	copyfile(path_to_hocr, dir_name + os.sep + sess_name + '.hocr')
 	copyfile(path_to_image, dir_name + os.sep + sess_name + '.jpg')
+
+def get_reading_times():
+	all_sesion_names = get_session_names()
+
+	all_times = []
+	for sess_name in all_sesion_names:
+		sess = Session(sess_name)
+		reading_times = [x for x in sess.metadata if x['part'] == 'digital reading']
+		if len(reading_times) == 0:
+			start_time = None
+			end_time = None
+		else:
+			start_time = min([x['start_time'] for x in reading_times])
+			end_time = max([x['end_time'] for x in reading_times])
+		all_times.append([sess_name, start_time, end_time])
+	with open('reading_times.csv', 'w') as outfile:
+		writer = csv.writer(outfile)
+		writer.writerow(['sess_name', 'start_time', 'end_time'])
+		for row in all_times:
+			writer.writerow(row)
 
 for sess_name in os.listdir('data'):
 	if sess_name.startswith('.') or sess_name.endswith('.zip') or sess_name.endswith('.txt') or sess_name == '4657':
